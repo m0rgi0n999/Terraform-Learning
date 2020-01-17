8a8bc232547e1a0a18f447147bf22c30dd823d2c
Erwin Rijvers <m0rgi0n999@gmail.com>
2020-01-17 09:58:36 +0100

    terraform init

---
+++ configuration.tf
@@ -0,0 +1,10 @@
+# Azure Provider
+provider "azurerm" {
+    version = "1.32.0"
+}
+
+# Create Resource Group
+resource "azurerm_resource_group" "rg" {
+    name = "myTFResourceGroup"
+    location = "westeurope"
+}
