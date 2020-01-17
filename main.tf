f3a289090dabab5cb09ef721aba832c0efb1e0a0
Erwin Rijvers <m0rgi0n999@gmail.com>
2020-01-17 13:07:02 +0100

    deps and stuff 4.6

--- main.tf
+++ main.tf
@@ -14,7 +14,7 @@ resource "azurerm_virtual_network" "vnet" {
     name                = "myTFVnet"
     address_space       = ["10.0.0.0/16"]
     location            = "westeurope"
-    resource_group_name = azurerm_resource_roup.rg.name
+    resource_group_name = azurerm_resource_group.rg.name
 }
 
 # Create subnet
